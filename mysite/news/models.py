5fcd4ef328141ad0a5dd7733781828d06c86712b
Anuar Umarov <anuar123@mail.ru>
2020-09-29 21:10:58 +0600

    new files end in django templates

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class News(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
@@ -8,3 +9,5 @@ class News(models.Model):
     photo = models.ImageField(upload_to='photos/%y/%m/%d')
     is_published = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.title
